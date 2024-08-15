f3bec3c475a3580feb646af515e896c0429f4e13
Facundo Figueroa <ffigueroa@nogadev.com>
2024-08-14 23:20:58 -0300

    3_ Creación de interfaces (contratos de los métodos)

---
+++ src/order-management/domain/interfaces/order.interfaces.ts
@@ -0,0 +1,7 @@
+import { Order } from "../order";
+// Acá se define lo que tiene que entrar en la clase que tenga este método y también lo que retorna.
+export interface IOrderRepository{
+  addOrder(order:Order): Promise<Order>; // Es un contrato a cumplir
+  getAllOrders(): Promise<Order[]>;
+  getOrderByOrderId(orderId: number): Promise<Order>;
+}
