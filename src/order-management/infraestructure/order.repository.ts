1505736f24a3c8e32a296009614dbb12123fa19a
Facundo Figueroa <ffigueroa@nogadev.com>
2024-08-15 21:13:16 -0300

    18_ Se crea un repository pero implementando prisma

--- src/order-management/infraestructure/order.repository.ts
+++ src/order-management/infraestructure/order.repository.ts
@@ -1,6 +1,7 @@
 import { createOrder, getOneOrder, getOrders } from "../../dependency-simulated";
 import { IOrderRepository } from "../domain/interfaces/order.interfaces";
 import { Order } from "../domain/order";
+import { prisma } from "../../prisma/index";
 
 export class OrderRepositoryDummyData implements IOrderRepository {
   public async addOrder(order: Order): Promise<Order> {
@@ -11,7 +12,23 @@ export class OrderRepositoryDummyData implements IOrderRepository {
     return getOrders();
   }
 
-  public async getOrderByOrderId (orderId: number): Promise<Order> {
+  public async getOrderByOrderId(orderId: number): Promise<Order> {
     return getOneOrder(orderId);
+  }
+}
+
+export class OrderRepositoryPrisma implements IOrderRepository {
+  public async addOrder(order: Order): Promise<Order> {
+    return prisma.order.create({
+      data: { productId: order.productId, total: order.total },
+    });
+  }
+
+  public async getAllOrders(): Promise<Order[]> {
+    return prisma.order.findMany();
+  }
+
+  public async getOrderByOrderId(orderId: number): Promise<Order | null > {
+    return prisma.order.findUnique({where: {orderId}});
   }
 }
