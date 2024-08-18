c2bb6b67154919297ca49a682ccbddc335ea292e
Facundo Figueroa <ffigueroa@nogadev.com>
2024-08-18 19:05:51 -0300

    6_ método post para los productos

---
+++ src/product-management/infraestructure/controllers/product.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Path,
+  Post,
+  Request,
+  Route,
+} from "tsoa";
+import { Express } from "express";
+import { ProductService } from "../../application/product.service";
+import { ImageService } from "../../application/image.service";
+import { ProductRepositoryPrisma } from "../product.repository";
+import { ImageRepositoryPrisma } from "../image.repository";
+import { DriveService } from "../../../google/drive-management/application/drive.service";
+import { DriveRepository } from "../../../google/drive-management/infraestructure/drive.repository";
+import { envs } from "../../../envs";
+import { DriveFileDTO } from "../../../google/drive-management/domain/drive.interface";
+
+@Route("products")
+export class ProductController extends Controller {
+  private readonly productService: ProductService;
+  private readonly imageService: ImageService;
+  private readonly driveService: DriveService;
+  constructor() {
+    super();
+    var productRepository = new ProductRepositoryPrisma();
+    var imageRepository = new ImageRepositoryPrisma();
+    var driveRepository = new DriveRepository();
+    this.productService = new ProductService(productRepository);
+    this.imageService = new ImageService(imageRepository);
+    this.driveService = new DriveService(driveRepository);
+  }
+  @Post()
+  public async addProduct(
+    @Body() requestBody: { name: string; price: number; description: string },
+    @Request() request: Express.Request
+  ) {
+    const files = request.files as Express.Multer.File[];
+    const driveDataSavedList: DriveFileDTO[] = await Promise.all(
+      files.map(async (file) => {
+        const fileSaved = await this.driveService.uploadFile(
+          file,
+          envs.GDRIVE_FOLDER_ID
+        );
+        return fileSaved;
+      })
+    );
+
+    const product = await this.productService.addProduct(
+      requestBody.name,
+      requestBody.description,
+      requestBody.price
+    );
+    console.log("driveDataSavedList => ", driveDataSavedList);
+    
+    const images = await this.imageService.createMultipleImages(
+      driveDataSavedList.map(
+        ({ fileId: storageId, webViewLink: imageUrl }) => ({
+          imageUrl,
+          storageId,
+          productId: product.id,
+        })
+      )
+    );
+    
+    return { ...product, images };
+  }
+}
