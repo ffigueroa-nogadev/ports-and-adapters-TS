937ee3eb892b7cc2e3cf374bff4018972b20fefa
Facundo Figueroa <ffigueroa@nogadev.com>
2024-08-18 19:02:02 -0300

    envs

--- src/envs/index.ts
+++ src/envs/index.ts
@@ -1,6 +1,11 @@
-import { load } from 'ts-dotenv';
+import { load } from "ts-dotenv";
 
 export const envs = load({
-    PORT: Number,
-    APP_NAME: String,
+  PORT: Number,
+  APP_NAME: String,
+  GDRIVE_CLIENT_ID: String,
+  GDRIVE_CLIENT_SECRET: String,
+  GDRIVE_REDIRECT_URI: String,
+  GDRIVE_REFRESH_TOKEN: String,
+  GDRIVE_FOLDER_ID: String,
 });
