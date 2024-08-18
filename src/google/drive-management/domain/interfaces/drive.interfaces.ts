1c389f2634a9cdc224eeda7ce1c7f653ec376776
Facundo Figueroa <ffigueroa@nogadev.com>
2024-08-18 18:55:57 -0300

    3_ configuración de interfaces para gdrive

---
+++ src/google/drive-management/domain/interfaces/drive.interfaces.ts
@@ -0,0 +1,8 @@
+import { Express } from "express";
+import { DriveFileDTO } from "../drive.interface";
+
+export interface IDriveFileRepository {
+  uploadFile(file: Express.Multer.File, filePath: string): Promise<DriveFileDTO>;
+  deleteFile(fileId: string): Promise<void>;
+  generatePublicUrl(fileId: string): Promise<DriveFileDTO>;
+}
