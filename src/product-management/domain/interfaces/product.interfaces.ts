83322ae3ce81f9bce42be115faff83e669321394
Facundo Figueroa <ffigueroa@nogadev.com>
2024-08-24 08:54:30 -0300

    1_ Updated UpdateProductDTO

--- src/product-management/domain/interfaces/product.interfaces.ts
+++ src/product-management/domain/interfaces/product.interfaces.ts
@@ -1,15 +1,11 @@
 import { Product } from "../product";
 
-export interface UpdateProductDTO {
-  name?: string;
-  description?: string;
-  price?: number;
-}
+export interface UpdateProductDTO extends Partial<Product> {}
 
-export interface IProductRepository{
-  addProduct(product:Product): Promise<Product>;
-  getAllProducts():Promise<Product[]>;
-  getProductById(id:number): Promise<Product>;
-  deleteProductById(id:number): Promise<Product>;
-  updateProductById(id:number, dto: UpdateProductDTO): Promise<Product>;
+export interface IProductRepository {
+  addProduct(product: Product): Promise<Product>;
+  getAllProducts(): Promise<Product[]>;
+  getProductById(id: number): Promise<Product>;
+  deleteProductById(id: number): Promise<Product>;
+  updateProductById(id: number, dto: UpdateProductDTO): Promise<Product>;
 }
