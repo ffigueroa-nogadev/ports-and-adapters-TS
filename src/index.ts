9ec123144cb6cb6cea319c12ae4974b1d53947a1
Facundo Figueroa <ffigueroa@nogadev.com>
2024-08-24 08:30:29 -0300

    7_ Added Cors Policy

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { json, Request, Response, urlencoded } from "express"
 import multer from "multer";
+import cors from "cors";
 import { RegisterRoutes } from "./build/routes";
 import { errorHandler } from "./middlewares/errorHandler";
 import { envs } from "./envs";
-
 const app = express()
 
+app.use(cors())
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage });
 app.use(upload.array('productImages', 5));
